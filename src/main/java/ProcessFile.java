69ee04f9b6727723d18d694782aa0eff60ccc615
BroEmilio <bro.emilio.1.1@gmail.com>
2026-01-23 15:16:14 +0100

    corrected set address

--- src/main/java/ProcessFile.java
+++ src/main/java/ProcessFile.java
@@ -117,7 +117,9 @@ public class ProcessFile {
 			// get individual person name
 			if(nameList[0].contains("Rodzice")){
 				ownerName += getNameIndyvidual(nameList[0]);
-				setAddress(nameList[1], owner);
+				if(nameList.length>1)
+					if(nameList[1] != null)
+						setAddress(nameList[1], owner);
 			} else
 				// get institutions names
 				if(! nameList[0].contains("małżeństwo")) {
